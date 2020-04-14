02d032e6750357a241d74466c7d828ffb75ac5d0
Nekika <hugo.jahnke@gmail.com>
2020-04-14 18:01:10 +0200

    Getting WFS capabilities, layers and features
    Adding the way to manage data from components using the store
    The getLayers action call the WFS to get data about layers and their
    features

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,43 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import WFS from '../API/WFS'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     state : {
-
+        capabilities: null,
+        layers: []
     },
     getters : {
-
+        capabilities: state => { return state.capabilities },
+        layers: state => { return state.layers }
     },
     mutations: {
-
+        setCapabilities: function (state, capabilities) {
+            state.capabilities = capabilities
+        },
+        setLayers: function (state, layers) {
+            state.layers = layers
+        },
+        addLayer: function (state, layer) {
+            state.layers.push(layer)
+        },
+        removeLayer: function (state, layer) {
+            const index = state.layers.indexOf(layer)
+            state.layers.splice(index, 1)
+        }
     },
     actions: {
-
+        getLayers: async function ({commit}) {
+            const capabilities = await WFS.getCapabilities()
+            const layers = WFS.extractLayers(capabilities)
+            for (const l of layers) {
+                l.entities = await WFS.getFeatures(l.Name._text)
+            }
+            commit('setCapabilities', capabilities)
+            commit('setLayers', layers)
+        }
     },
     modules : {
 
