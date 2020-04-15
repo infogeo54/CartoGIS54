1f343e91bdacd4088b1bad326a27e28efcc8afbe
Nekika <hugo.jahnke@gmail.com>
2020-04-15 16:39:19 +0200

    Creating the Map component
    This component displays a map and some markers, lines or polygons using
    Leaflet

---
+++ src/components/Map.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="map"></div>
+</template>
+
+<script>
+    import L from 'leaflet'
+    export default {
+        name: "Map",
+        data() {
+            return {
+                map: null
+            }
+        },
+        methods: {
+            addMarker: function () {
+
+            },
+            init: function (center) {
+                this.map = L.map('map').setView(center, 15)
+                L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+                    // Il est toujours bien de laisser le lien vers la source des données
+                    attribution: 'données © OpenStreetMap/ODbL - rendu OSM France',
+                    minZoom: 1,
+                    maxZoom: 20
+                }).addTo(this.map)
+            },
+        },
+        mounted() {
+            this.init([49.305, 5.78])
+        }
+    }
+</script>
+
+<style lang="sass" scoped>
+   #map
+        height: 100%
+        width: 100%
+</style>
