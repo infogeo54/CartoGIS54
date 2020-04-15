a4f2f2b5e33a3d09ae4ccfec3d65c7ba2b9bd4c1
Nekika <hugo.jahnke@gmail.com>
2020-04-15 16:39:31 +0200

    Adding Menu & Map components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
+    <Menu></Menu>
+    <Map></Map>
   </div>
 </template>
 
 <script>
+  import Menu from './components/Menu'
+  import Map from './components/Map'
   import {mapGetters, mapActions} from 'vuex'
   export default {
     name: 'App',
@@ -22,10 +26,17 @@
     },
     mounted() {
       this.getLayers()
+    },
+    components: {
+      Menu,
+      Map
     }
   }
 </script>
 
-<style>
-
+<style lang="sass" scoped>
+  #app
+    height: 100vh
+    display: flex
+    flex-direction: row
 </style>
