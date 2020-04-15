8f97438a8981237e033c104f3ee9897b0f196692
Nekika <hugo.jahnke@gmail.com>
2020-04-15 18:08:14 +0200

    Adding markers
    Using leaflet to add markers and polygons to represent layers' features

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -18,8 +18,24 @@
             })
         },
         methods: {
-            addMarker: function () {
-
+            addToMap: function (feature) {
+                let coordinates = feature.geometry.coordinates
+                let representation
+                if (coordinates.length === 2 ) {
+                    coordinates = coordinates.reverse() // WFS sends reversed coordinates
+                    representation = L.marker(coordinates)
+                } else {
+                    let polygon = coordinates[0][0] // WFS sends an array of reversed coordinates
+                    let points = []
+                    for (let p of polygon) {
+                        p = p.reverse()
+                        points.push(p)
+                    }
+                    representation = L.polygon(points, {fillOpacity: 0.5})
+                    console.log(representation)
+                }
+                representation.bindPopup(`${feature.id}`)
+                .addTo(this.map)
             },
             init: function (center) {
                 this.map = L.map('map').setView(center, 15)
@@ -29,6 +45,12 @@
                     minZoom: 1,
                     maxZoom: 20
                 }).addTo(this.map)
+                this.layers.forEach(l => {
+                    const features = l.entities.features
+                    features.forEach(f => {
+                        this.addToMap(f)
+                    })
+                })
             },
         },
         mounted() {
