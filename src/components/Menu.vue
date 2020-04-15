3f1c0f7037d9959f96d843a081d52499b72cd860
Nekika <hugo.jahnke@gmail.com>
2020-04-15 16:33:53 +0200

    Creating Menu & Legend components
    The menu component is the list of project's available layers
    It includes the Legend sub-component to display layers' features icons

---
+++ src/components/Menu.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="menu">
+        <div id="categories">
+            <h3>Catégories</h3>
+            <ul>
+                <li v-for="(layer, index) in layers" :key="index">{{ getTitle(layer) }}</li>
+            </ul>
+        </div>
+        <Legend></Legend>
+    </div>
+</template>
+
+<script>
+    import Legend from './Legend'
+    import {mapGetters} from 'vuex'
+    export default {
+        name: "Menu",
+        computed: {
+            ...mapGetters({
+                layers: 'layers'
+            })
+        },
+        methods: {
+            getTitle: function (layer) {
+                return layer.Title._text
+            }
+        },
+        components: {
+            Legend
+        },
+    }
+</script>
+
+<style lang="sass" scoped>
+    #menu
+        text-align: center
+        width: 18%
+        ul
+            padding: 0
+            li
+                list-style: none
+                &:hover
+                    text-decoration: underline
+                    cursor: pointer
+
+</style>
