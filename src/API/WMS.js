d32aa6064d8abe20141fb9da502cc45cc92a66e1
Nekika <hugo.jahnke@gmail.com>
2020-04-16 17:07:41 +0200

    Creating the WMS module
    This modules allows to get WMS informations from the server
    It will be used to retrieve informations about layers styles (= features
    icons)

--- src/API/WMS.js
+++ src/API/WMS.js
@@ -1,1 +1,25 @@
+import axios from 'axios'
+import * as convert from 'xml-js'
 
+const host = 'localhost:8080'
+const baseUrl = `http://${host}?SERVICE=WMS&VERSION=1.3.0`
+
+async function getCapabilities() {
+    const url = `${baseUrl}&REQUEST=GetCapabilities`
+    const res = await axios.get(url)
+    return res.data
+}
+
+function extractStyles(stylesXML) {
+    const stylesJS = convert.xml2js(stylesXML, {compact: true, spaces: 4})
+    return stylesJS['StyledLayerDescriptor']['NamedLayer']['UserStyle']['se:FeatureTypeStyle']['se:Rule']
+
+}
+
+async function getStyles(layerName) {
+    const url = `${baseUrl}&REQUEST=GetStyles&LAYERS=${layerName}`
+    const res = await axios.get(url)
+    return extractStyles(res.data)
+}
+
+export default {getCapabilities, getStyles, extractStyles}
