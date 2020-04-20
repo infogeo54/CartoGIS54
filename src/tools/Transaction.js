e5024cdd4164942ffb53c7bbee9eef8b3e265b07
Nekika <hugo.jahnke@gmail.com>
2020-04-20 16:54:13 +0200

    Implementing Transaction constructor
    A transaction object is formatted following xml-js object structure
    Trying to add Insert Update and Delete tags to all requests even if two
    of them are empty because I want to see if it raises an error

--- src/tools/Transaction.js
+++ src/tools/Transaction.js
@@ -2,18 +2,37 @@
     Build a Transaction
  */
 
+
 class Transaction{
 
     constructor() {
+        this['_declaration'] = {
+            '_attributes': {
+                'version': '1.0',
+                'encoding': 'utf-8'
+            }
+        }
+        this['wfs:Transaction'] = {
+            '_attributes':  {
+                'version': '1.3.0',
+                'xmlns:topp': 'http://www.openplans.org/topp',
+                'xmlns:fes': 'http://www.opengis.net/fes/2.0',
+                'xmlns:gml': 'http://www.opengis.net/gml/3.2',
+                'xmlns:wfs': 'http://www.opengis.net/wfs/2.0',
+                'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance'
+            },
+            'wfs:Insert': {},
+            'wfs:Update': {},
+            'wfs:Delete': {},
+        }
     }
 
     static insert (layer, feature) {
-
     }
 
     static update (layer, feature) {
     }
 
-    static delete (layer, feature) {
+    static delete (feature) {
     }
 }
