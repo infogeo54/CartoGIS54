37692adf27740c20af94e64a3583f0f7114f2be9
Nekika <hugo.jahnke@gmail.com>
2020-04-20 11:45:30 +0200

    Adding layer icons description
    When a layername is clicked from the menu, the corresponding layer is
    passed as a prop to the Legend component
    The Legend is used to describe the different styles of this layer
    Later you'll be able to click on a style to an entity

--- src/components/Legend.vue
+++ src/components/Legend.vue
@@ -1,15 +1,58 @@
 <template>
     <div id="legend">
-
+        <h3>{{ layerName }}</h3>
+        <ul>
+            <li v-for="(style, index) in layer.styles" :key="index">
+                <img :src="getStyleIcon(style)" :alt="getStyleName(style)">
+                <h4>{{ getStyleName(style) }}</h4>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Legend"
+        name: "Legend",
+        props: ['layer'],
+        computed: {
+            layerName: function() {
+                return this.layer.Title._text
+            }
+        },
+        methods: {
+            /**
+             * Get the name of a style
+             */
+            getStyleName: function(style) {
+                return style['se:Name']['_text']
+            },
+            /**
+             * Get a style's corresponding icon
+             */
+            getStyleIcon: function(style) {
+                const name = this.getStyleName(style).toLowerCase()
+                let icon
+                try {
+                    icon = require(`../assets/icons/${name}.svg`)
+                } catch (e) {
+                    icon = require('../assets/icons/poi.svg')
+                }
+                return icon
+            }
+        }
     }
 </script>
 
-<style scoped>
-
+<style lang="sass" scoped>
+    #legend
+        ul
+            padding: 0
+            li
+                list-style: none
+                h4
+                    font-weight: normal
+                    margin: 0
+                img
+                    max-width: 64px
+                    height: auto
 </style>
