756ab555c246125d98362335b1c5f3a38063b71e
Nekika <hugo.jahnke@gmail.com>
2020-04-20 14:26:49 +0200

    Adding createIcon and representation methods
    A 'representation' is the graphical representation of a feature.
    For a point we display a marker and for a polygon we display a polygon
    
    The createIcon method is used by markers to display the feature type
    corresponding icon

--- src/tools/MapTools.js
+++ src/tools/MapTools.js
@@ -5,9 +5,31 @@
 import L from 'leaflet'
 
 export default {
-    createMarker: function (feature) {
-        return L.marker(feature.geometry.coordinates.reverse()) // WFS sends reversed coordinates
+    createIcon: function (feature) {
+        let icon
+        try {
+            icon = require(`../assets/icons/${feature.properties.type}.svg`)
+        } catch (e) {
+            console.log(e)
+            icon = require('../assets/icons/poi.svg')
+        }
+        return L.icon({
+            iconUrl: icon,
+            iconSize: [48, 105],
+            iconAnchor: [30, 102],
+            popupAnchor: [-3, -76],
+        })
     },
+    /**
+     * Create a Leaflet marker representation
+     */
+    createMarker: function (feature, coordinates) {
+        const icon = this.createIcon(feature)
+        return L.marker(coordinates, {icon: icon})
+    },
+    /**
+     * Create a Leaflet poylgon representation
+     */
     createPolygon: function (coordinates) {
         let polygon = coordinates[0][0] // WFS sends an array of reversed coordinates
         let points = []
@@ -17,6 +39,9 @@ export default {
         }
         return L.polygon(points, {fillOpacity: 0.5})
     },
+    /**
+     * Create a representation's popup
+     */
     createPopUp: function (feature) {
         const props = feature.properties
         return `<h3>${props.nom}</h3>
@@ -26,5 +51,19 @@ export default {
                         <td>${props.type}</td>
                     </tr>
                 </table>`
+    },
+    /**
+     * Build a feature's representation (marker or polygon)
+     */
+    representation: function (feature) {
+        let rep
+        const coordinates = feature.geometry.coordinates.reverse() // WFS sends reversed coordinates
+        if (coordinates.length === 2) {
+            rep = this.createMarker(feature, coordinates)
+        } else {
+            rep = this.createPolygon(coordinates)
+        }
+        const popup = this.createPopUp(feature)
+        return rep.bindPopup(popup)
     }
 }
