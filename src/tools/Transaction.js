cfcca11a57f4a02ac6217bb170d9e2afbf4e14d6
Nekika <hugo.jahnke@gmail.com>
2020-04-20 16:51:00 +0200

    Creating the Transaction class
    This class will help to build WFS Transaction body in order to Insert,
    Delete or Update features
    A Transaction instance will be an object which will be parsed into XML,
    using the xml-js module

---
+++ src/tools/Transaction.js
@@ -0,0 +1,19 @@
+/*
+    Build a Transaction
+ */
+
+class Transaction{
+
+    constructor() {
+    }
+
+    static insert (layer, feature) {
+
+    }
+
+    static update (layer, feature) {
+    }
+
+    static delete (layer, feature) {
+    }
+}
