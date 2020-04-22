a11e23d67d36a77ece1ba17334e068ecc3c48108
Nekika <hugo.jahnke@gmail.com>
2020-04-22 18:13:36 +0200

    Adding FormGroup component

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,17 +1,31 @@
 <template>
     <div id="form">
-
+        <FormGroup v-for="(value, property) in feature.properties"
+                   :key="property"
+                   :property="property"
+                   :value="value"
+                   @changed="onChange"></FormGroup>
     </div>
 </template>
 
 <script>
-    import {mapGetters} from 'vuex'
+    import FormGroup from './FormGroup'
+    import {mapGetters, mapMutations} from 'vuex'
     export default {
         name: "Form",
         computed: {
             ...mapGetters({
                 feature: 'feature/selected'
             })
+        },
+        methods: {
+            ...mapMutations('feature', ['setSelected']),
+            onChange: function () {
+                this.setSelected(this.feature)
+            }
+        },
+        components: {
+            FormGroup
         }
 
     }
