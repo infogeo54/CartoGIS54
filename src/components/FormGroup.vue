8201da5427ea52e5046447ca88995456f311b5f3
Nekika <hugo.jahnke@gmail.com>
2020-04-22 18:09:52 +0200

    Creating FormGroup component
    This component is used to display a from group (label + input) in the
    Form component

---
+++ src/components/FormGroup.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="form-group">
+        <label :for="id">{{ title }}</label>
+        <input :id="id" :type="type" v-model="value" @change="changed">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FormGroup",
+        props: ['property', 'value'],
+        computed: {
+            type: function () {
+                const regex = new RegExp(/^.*date.*$/)
+                return this.property.match(regex) ? 'date' : 'text'
+            },
+            title: function () {
+                const words = this.property.split('_').join(' ')
+                return words[0].toUpperCase() + words.slice(1)
+            },
+            id: function () {
+                return `ipt-${this.property}`
+            }
+        },
+        methods: {
+            changed:function () {
+                this.$emit('changed')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
