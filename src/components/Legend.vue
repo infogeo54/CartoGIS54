6e7aac1e257de07e884cb247f8a31e2c9c25edfb
Nekika <hugo.jahnke@gmail.com>
2020-04-22 18:12:56 +0200

    Adding LegendItem component

--- src/components/Legend.vue
+++ src/components/Legend.vue
@@ -1,72 +1,46 @@
 <template>
     <div id="legend">
-        <h3>{{ layerTitle }}</h3>
-        <ul>
-            <li v-for="(style, index) in layer.styles"
-                :key="index"
-                @click="onClick"
-            >
-                <img :src="getStyleIcon(style)" :alt="getStyleName(style)">
-                <h4>{{ getStyleName(style) }}</h4>
-            </li>
-        </ul>
+        <h3>{{ title }}</h3>
+        <LegendItem v-for="(style, index) in layer.styles"
+                    :key="index"
+                    :feature-style="style"
+                    @clicked="onClick"></LegendItem>
     </div>
 </template>
 
 <script>
+    import LegendItem from './LegendItem'
     import {mapActions} from 'vuex'
     export default {
         name: "Legend",
         props: ['layer'],
         computed: {
-            layerTitle: function() {
+            title: function() {
                 return this.layer.Title._text
             },
-            layerName: function () {
+            name: function () {
                 return this.layer.Name._text
             }
         },
         methods: {
             ...mapActions('feature', ['getSchema']),
             /**
-             * Get the name of a style
-             */
-            getStyleName: function(style) {
-                return style['se:Name']['_text']
-            },
-            /**
-             * Get a style's corresponding icon
-             */
-            getStyleIcon: function(style) {
-                const name = this.getStyleName(style).toLowerCase()
-                let icon
-                try {
-                    icon = require(`../assets/icons/${name}.svg`)
-                } catch (e) {
-                    icon = require('../assets/icons/poi.svg')
-                }
-                return icon
-            },
-            /**
              * Call getSchema
              */
-            onClick: function () {
-                this.getSchema(this.layerName)
+            onClick: function (styleName) {
+                const params = {
+                    layer: this.name,
+                    style: styleName
+                }
+                this.getSchema(params)
             }
+        },
+        components: {
+            LegendItem
         }
     }
 </script>
 
 <style lang="sass" scoped>
-    #legend
-        ul
-            padding: 0
-            li
-                list-style: none
-                h4
-                    font-weight: normal
-                    margin: 0
-                img
-                    max-width: 64px
-                    height: auto
+
 </style>
