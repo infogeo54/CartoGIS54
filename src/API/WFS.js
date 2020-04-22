3112a82f1114b555443421ad11d7bc753d9fb11a
Nekika <hugo.jahnke@gmail.com>
2020-04-22 13:28:34 +0200

    Adding getFeatureDescription & extractSchema
    The getFeature method makes a DescribeFeature Request to get the layer's
    data structure
    The extractSchema method simply extracts the data structure from a
    stringified DescribeFeature XML document

--- src/API/WFS.js
+++ src/API/WFS.js
@@ -1,11 +1,14 @@
 import axios from 'axios'
 import * as convert from 'xml-js'
 
+//const axios = require('axios')
+//const convert = require('xml-js')
+
 const host = 'localhost:8080'
 const baseUrl = `http://${host}?SERVICE=WFS&VERSION=1.3.0&OUTPUTFORMAT=GEOJSON`
 
 /**
- * Make a GetCapabilities AJAX request and return the response
+ * Make a GetCapabilities AJAX request and return a stringified XML document from the response
  * @param host - The request's target
  * @returns {Promise<AxiosResponse<any>>} - The response
  */
@@ -17,17 +20,52 @@ async function getCapabilities() {
 
 /**
  * Extract a list of layers from a Capabilities XML document
- * @param capabilitiesXML - Capabilities XML document
+ * @param capabilitiesXML : String - Capabilities stringified XML document
  */
 function extractLayers(capabilitiesXML) {
-    const capabilities = convert.xml2js(capabilitiesXML, {compact: true, spaces: 4})
+    const capabilities = convert.xml2js(capabilitiesXML, {compact: true})
     return capabilities.WFS_Capabilities.FeatureTypeList.FeatureType
 }
 
+/**
+ * Make a GetFeature AJAX request and return the data from the response
+ * @param layer : String - The name of the associated layer
+ * @returns Object
+ */
 async function getFeatures(layer) {
     const url = `${baseUrl}&REQUEST=GetFeature&TYPENAME=${layer}`
     const res = await axios.get(url)
     return res.data
 }
 
-export default {getCapabilities, extractLayers, getFeatures}
+/**
+ * Make a DescribeFeature AJAX request and return a stringified XML document from the response
+ * @param layer : String - The name of the associated layer
+ * @returns String
+ */
+async function getFeatureDescription(layer) {
+    const url = `${baseUrl}&REQUEST=DescribeFeatureType&TYPENAME=${layer}`
+    const res = await axios.get(url)
+    return res.data
+}
+
+/**
+ * Extract a list of property objects from a Feature Description XML document
+ * @param descriptionXML : String - Feature Description stringified XML document
+ * @returns Array
+ */
+function extractSchema(descriptionXML) {
+    const description = convert.xml2js(descriptionXML, {compact: true})
+    const elements = description['schema']['complexType']['complexContent']['extension']['sequence']['element']
+    return elements.map(e => { return e['_attributes']})
+}
+
+export default {
+    getCapabilities,
+    extractLayers,
+    getFeatures,
+    getFeatureDescription,
+    extractSchema
+}
+
+//module.exports = {getFeatureDescription, extractAttributes}
