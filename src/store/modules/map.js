5a24b52d1dc129439d3d226cb17831711628dbbf
Nekika <hugo.jahnke@gmail.com>
2020-04-27 22:27:16 +0200

    Removing map module coordinates attribute
    This attribute is now useless

--- src/store/modules/map.js
+++ src/store/modules/map.js
@@ -2,24 +2,18 @@ export default {
     namespaced: true,
     state: {
         editing: false,
-        coordinates: null
     },
     getters: {
         editing: state => { return state.editing },
-        coordinates: state => { return state.coordinates }
     },
     mutations: {
         setEditing: function (state, editing) {
             state.editing = editing
-        },
-        setCoordinates: function (state, coordinates) {
-            state.coordinates = coordinates
         }
     },
     actions: {
         reset: function ({commit}) {
             commit('setEditing', false)
-            commit('setCoordinates', null)
         }
     }
 }
