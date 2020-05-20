7eec320a85535620acd42f494ddeb8bb226e7bfd
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-05-20 16:20:58 +0200

    Adding some getters and setters

--- src/models/Feature.js
+++ src/models/Feature.js
@@ -8,19 +8,33 @@ export default class Feature {
     }) {
         this.id = options.id
         this.parent = options.parent
-        this.setProperties(options.parent, options.properties)
-        this.representation = MapTools.representation(this)
+        this._properties = {parent: options.parent, props: options.properties}
     }
 
-    setProperties (parent, props) {
-        const attributes = parent.description.attributes
+    set _properties (params) {
+        const attributes = params.parent.description.attributes
         let properties = {}
         for (let attr in attributes) {
             const type = attributes[attr].type
-            let value = props[attr]
+            let value = params.props[attr]
             if (type === 'boolean' && !value) value = 'FALSE' // New Feature's booleans are init at FALSE
             properties[attr] = {type: type, value: value}
         }
         this.properties = properties
+    }
+
+    set coordinates (coordinates) {
+        this.properties.geometry.value = {coordinates: coordinates}
+    }
+
+    get coordinates () {
+        if(this.properties.geometry.value) return this.properties.geometry.value.coordinates
+        return null
+    }
+
+    createRepresentation () {
+        if (this.coordinates) {
+            this.representation = MapTools.representation(this)
+        }
     }
 }
