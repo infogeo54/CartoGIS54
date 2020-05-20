f0cab753f9ab3b056624fe164fc60d1872fed751
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-05-20 15:04:12 +0200

    Layer module doesnt manage descriptions anymore
    We don't need to manage descriptions from the store anymore since they are linked to layers once one they are created

--- src/store/modules/layer.js
+++ src/store/modules/layer.js
@@ -6,16 +6,11 @@ export default {
     state : {
         list: [],
         selected: {},
-        descriptions: []
     },
     getters : {
         list: state => state.list,
         selected: state => state.selected,
-        features: state => state.list.map(l => l.features).flat(),
-        descriptions: state => state.descriptions,
-        getDescription: (state) => (layer) => {
-            return state.descriptions.find(d => d.layer === layer.name)
-        }
+        features: state => state.list.map(l => l.features).flat()
     },
     mutations: {
         setList: function (state, layers) {
@@ -30,9 +25,6 @@ export default {
         },
         setSelected: function (state, layer) {
             state.selected = layer !== state.selected ? layer : null
-        },
-        setDescriptions: function (state, descriptions) {
-            state.descriptions = descriptions
         },
         /**
          * After an Insert Transacation, add a feature to it's parent layer
@@ -57,22 +49,19 @@ export default {
     },
     actions: {
         getLayers: async function ({commit}) {
-            const capabilities = await WFS.fetchCapabilities()
-            const layersData = WFS.extractLayers(capabilities)
+            const layerList = await WFS.fetchLayers()
+            const descriptionList = await WFS.fetchAllFeatureDescriptions()
             const layers = await Promise.all(
-                layersData.map(async data => {
-                    const layer = new Layer(data)
+                layerList.map(async layerData => {
+                    const name = layerData['Name']['_text']
+                    const description = descriptionList.find(d => d.layer === name)
+                    const layer = new Layer({properties: layerData, description: description})
                     await layer.getFeatures()
                     await layer.getStyles()
                     return layer
                 })
             )
-            commit('setCapabilities', capabilities, {root: true})
             commit('setList', layers)
-        },
-        getDescriptions: async function ({commit}) {
-            const descriptions = await WFS.fetchAllFeatureDescriptions()
-            commit('setDescriptions', descriptions)
         },
         reset: function ({commit}) {
             commit('setList', [])
