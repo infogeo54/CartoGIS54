8c4c590654718b46b0911ae4d075e319a23180ea
Nekika <hugo.jahnke@gmail.com>
2020-05-25 11:17:40 +0200

    Editing template & computed properties
    - Template : Placing loader first (makes more sense) & using auto
    closing components tags
    - Computed props : Removing useless capabilities getter & rewriting
    formVisibile in order to display the form only when the selectedFeature
    representation exists

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,55 @@
 <template>
-  <div v-if="!loading" id="app">
-    <Menu></Menu>
-    <Map></Map>
-    <Form v-if="formVisible"></Form>
+  <Loader v-if="loading" />
+  <div v-else id="app">
+    <Menu />
+    <Map />
+    <Form v-if="formVisible" />
   </div>
-  <Loader v-else></Loader>
 </template>
 
 <script>
-  import Loader from './components/Loader'
-  import Menu from './components/Menu'
-  import Map from './components/Map'
-  import Form from './components/Form'
-  import {mapGetters, mapActions} from 'vuex'
-  export default {
-    name: 'App',
-    data() {
-      return {
-      }
+import Loader from './components/Loader'
+import Menu from './components/Menu'
+import Map from './components/Map'
+import Form from './components/Form'
+import {mapGetters, mapActions} from 'vuex'
+
+export default {
+  name: 'App',
+  computed: {
+    ...mapGetters({
+      layers: 'layer/list',
+      feature: 'feature/selected',
+    }),
+    loading: function () {
+      return !this.layers.length
     },
-    computed: {
-      ...mapGetters({
-        capabilities: 'capabilities',
-        layers: 'layer/list',
-        feature: 'feature/selected'
-      }),
-      loading: function () {
-        return !this.layers.length
-      },
-      formVisible: function () {
-        if (this.feature) return this.feature.coordinates
-        return false
-      }
-    },
-    methods: {
-      ...mapActions('layer', ['getLayers']),
-    },
-    mounted() {
-      this.getLayers()
-    },
-    components: {
-      Loader,
-      Menu,
-      Map,
-      Form
+    formVisible: function () {
+      if (this.feature) return !!this.feature.representation
+      return false
     }
+  },
+  methods: {
+    ...mapActions('layer', ['getLayers']),
+  },
+  mounted() {
+    this.getLayers()
+  },
+  components: {
+    Loader,
+    Menu,
+    Map,
+    Form
   }
+}
 </script>
 
 <style lang="sass">
-    body
-        height: 100vh
-        margin: 0
-    #app
-        height: 100%
-        display: flex
-        flex-direction: row
+  body
+      height: 100vh
+      margin: 0
+  #app
+      height: 100%
+      display: flex
+      flex-direction: row
 </style>
