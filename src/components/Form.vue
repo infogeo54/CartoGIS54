81507015ed05c320737c954fa903f64db6f3a536
Nekika <hugo.jahnke@gmail.com>
2020-05-28 16:39:42 +0200

    Single tag component

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,9 +1,10 @@
 <template>
     <form id="form" @submit.prevent="submitForm">
-        <FormGroup v-for="(property, index) in properties"
-                   :key="index"
+        <FormGroup v-for="property in properties"
+                   :key="property"
                    :property="{name: property, ...feature.properties[property]}"
-                   @changed="onChange"></FormGroup>
+                   @changed="onChange"
+        />
         <button name="save">Enregistrer</button>
         <button name="delete">Supprimer</button>
         <button type="button" name="cancel" @click="onCancelClick">Annuler</button>
@@ -11,70 +12,67 @@
 </template>
 
 <script>
-    import FormGroup from './FormComponents/FormGroup'
-    import {mapGetters, mapMutations, mapActions} from 'vuex'
-    export default {
-        name: "Form",
-        data(){
-            return {
-                element: null
-            }
-        },
-        computed: {
-            ...mapGetters({
-                layer: 'layer/selected',
-                feature: 'feature/selected',
-            }),
-            properties: function () {
-                return Object.keys(this.feature.properties).sort((a, b) =>
-                    a < b ? -1 : a > b ? 1 : 0
-                )
-            },
-        },
-        methods: {
-            ...mapMutations('feature', ['updateAttribute']),
-            ...mapActions([
-                'reset',
-                'feature/save',
-                'feature/delete'
-            ]),
-            /**
-             * Saves changes in the Store
-             * @param attribute
-             */
-            onChange: function (attribute) {
-                this.updateAttribute(attribute)
-            },
-            /**
-             * Sends a Transaction request
-             */
-            onSaveClick: async function () {
-                await this['feature/save'](this.layer.name)
-                this.$destroy()
-            },
-            onCancelClick: function () {
-                if (!this.feature.id) this.feature.representation.remove()
-                this.$destroy()
-            },
-            onDeleteClick: async function () {
-                await this['feature/delete']()
-                this.$destroy()
-            },
-            submitForm: function (e) {
-                return e.submitter.name === 'save' ? this.onSaveClick() : this.onDeleteClick()
-            }
-        },
-        mounted() {
-            this.element = this.$el
-        },
-        beforeDestroy() {
-            this.reset()
-        },
-        components: {
-            FormGroup
-        }
+import FormGroup from './FormComponents/FormGroup'
+import {mapGetters, mapMutations, mapActions} from 'vuex'
 
+export default {
+    name: "Form",
+    components: { FormGroup },
+    data () {
+        return {
+            element: null
+        }
+    },
+    computed: {
+        ...mapGetters({
+            layer: 'layer/selected',
+            feature: 'feature/selected'
+        }),
+        properties: function () {
+            return Object.keys(this.feature.properties).sort((a, b) => a < b ? -1 : a > b ? 1 : 0)
+        },
+    },
+    methods: {
+        ...mapMutations('feature', ['updateAttribute']),
+        ...mapActions([
+            'reset',
+            'feature/save',
+            'feature/delete'
+        ]),
+        /**
+         * Saves changes in the Store
+         * @param attribute
+         */
+        onChange: function (attribute) {
+            this.updateAttribute(attribute)
+        },
+        /**
+         * Sends a Transaction request
+         */
+        onSaveClick: async function () {
+            await this['feature/save'](this.layer.name)
+            this.$destroy()
+        },
+        onCancelClick: function () {
+            this.feature.representation.remove()
+            this.$destroy()
+        },
+        onDeleteClick: async function () {
+            await this['feature/delete']()
+            this.$destroy()
+        },
+        submitForm: function (e) {
+            return e.submitter.name === 'save' ? this.onSaveClick() : this.onDeleteClick()
+        }
+    },
+    mounted() {
+        this.element = this.$el
+    },
+    beforeDestroy() {
+        this.reset()
     }
+
+}
 </script>
 
 <style scoped>
