d5e2eddcecc15b6c4129cb8544650c3f21438562
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-06-18 10:28:38 +0200

    Creating default layout

---
+++ src/layouts/Default.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="default-layout">
+        <TopBar />
+        <slot />
+    </div>
+</template>
+
+<script>
+import TopBar from '@/components/TopBar'
+
+export default {
+    name: 'default-layout',
+    components: {
+        TopBar
+    }
+}
+</script>
+
+<style lang="sass">
+    html
+      height: 100vh
+    body
+      margin: 0
+      height: 100%
+    .default-layout
+      height: 100%
+      display: grid
+      grid-template-rows: repeat(10, 10%)
+    .top-bar
+      grid-row: 1 / 2
+    main
+      grid-row: 2 / 11
+</style>
