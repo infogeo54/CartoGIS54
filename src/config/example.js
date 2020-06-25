6f85a549ab407377e3d5074e4d2e7d55d1757da3
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-06-25 16:06:23 +0200

    Config - Adding more examples

--- src/config/example.js
+++ src/config/example.js
@@ -1,7 +1,37 @@
+/**
+ * These options are used to configure the the URL where to send HTTP requests
+ * host : the server's IP address or URL
+ * queryParams : an array of default query parameters, applied to each requests
+ */
 export const server = {
-    host: "Your host : 0.0.0.0",
+    host: "0.0.0.0" || "my.geo.server.fr",
     queryParams: [
-        "Your default query parameteres : ",
-        "SERVICE=WMS"
+        "SERVICE=WMS",
+        "MAP=/path/to/your/qgis/project.qgs"
     ]
+}
+
+/**
+ * These options are used in order to add more customization to the interface
+ * form : options relative to the Form component and it's subcomponents
+ */
+export const app = {
+    form: {
+        groups: {
+            textAreaFields: [
+                'observations'
+            ],
+            requiredFields: [
+                'nom',
+                'projet',
+                'geometry',
+                'disponible',
+                'vacances'
+            ],
+            disabledFields: [
+                'type',
+                'geometry'
+            ]
+        }
+    }
 }
