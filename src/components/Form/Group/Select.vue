f4089bc1a569af8e6e5399289ba4d50047965146
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-07-06 15:23:20 +0200

    Creating Select component
    This component will be used to render select boxes within the Form component.
    It accepts a field prop to determine options.

---
+++ src/components/Form/Group/Select.vue
@@ -0,0 +1,40 @@
+<template>
+  <select
+      :value="value"
+      @change="change"
+  >
+    <option
+        v-for="(value, text) in options"
+        :key="text"
+    >
+      {{ text }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+    props: {
+        value: { type: [String, Number, Boolean], default: () => '' },
+        field: { type: Object, default: () => {} }
+    },
+    computed: {
+        options () { return this.field.options.map },
+    },
+    methods: {
+        change (e) { this.$emit('change', e) }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+select
+  cursor: pointer
+  background-color: #E6E6E6
+  border: solid 1px black
+  border-radius: 4px
+  padding: 4px 0
+  text-align: center
+  &[disabled]
+    cursor: not-allowed
+</style>
