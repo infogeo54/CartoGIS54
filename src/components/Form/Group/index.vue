2a8960df02da79978788ba9222cb01b8584a12a4
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-07-08 11:07:36 +0200

    Form - Implementing Custom and Default Groups
    The Group component is now used to determine if a given property has to be rendered inside a a Custom or Default Group

--- src/components/Form/Group/index.vue
+++ src/components/Form/Group/index.vue
@@ -1,55 +1,41 @@
 <template>
-  <div v-if="category" class="form-group">
-    <label>{{ title }}</label>
-    <textarea
-        v-if="category === 'textArea'"
-        :value="value"
-        @change="changed"
-    >
-    </textarea>
-    <Select
-        v-else-if="category === 'selectBox'"
-        :value="value"
-        :field="field"
-        @change="changed"
-    />
-    <Input
-        v-else
-        :category="category"
-        :value="value"
-        :field="field"
-        @change="changed"
-    />
-  </div>
+  <Custom
+      v-if="isCustom"
+      :property="property"
+      class="form-group"
+  />
+  <Default
+      v-else
+      :property="property"
+      class="form-group"
+  />
 </template>
 
 <script>
 import { form as config } from '@/config/app.config.json'
-import Select from './Select'
-import Input from './Input'
+import _ from 'lodash'
+import Default from './Default'
+import Custom from './Custom'
 
 export default {
-    components: { Select, Input },
+    components: { Default, Custom },
     props: {
         property: { type: Object, default: null }
     },
     computed: {
-        category () {
-            let res
-            for (let category in config) {
-                const field = config[category].find(f => f.name === this.property.name)
-                if (field) { res = category }
-            }
-            return res
+        isConfigFileConfigured () { return (!!config && !_.isEmpty(config)) },
+        isCustom () {
+            if (!this.isConfigFileConfigured) { return false }
+            let matchingField = null
+            const categories = Object.keys(config)
+            categories.forEach(c => {
+                const fields = config[c]
+                fields.forEach(f => {
+                    if (f.name === this.property.name) { matchingField = f}
+                })
+            })
+            return !!matchingField
         },
-        field () { return config[this.category].find(field => field.name === this.property.name) },
-        title: function () {
-           return this.field.alias ? this.field.alias : this.field.name.toUpperCase()
-        },
-        value: function () {
-            if (this.property.name !== 'geometry') return this.property.value || this.field.default
-            return this.property.value.coordinates.join(', ')
-        }
     },
     methods: {
         changed (e) {
@@ -62,16 +48,27 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="sass">
 .form-group
   display: flex
   flex-direction: column
   text-align: center
   margin: 6px 0
-  input, textarea, select
+  & > *
     width: 90%
     max-width: 300px
+    text-align: center
     margin: auto
+  & > input
+    width: 90%
+    max-width: 300px
+    background-color: #E6E6E6
+    border: solid 1px black
+    border-radius: 4px
+    padding: 4px 0
+    text-align: center
+    &[disabled]
+      cursor: not-allowed
   textarea
     background-color: #E6E6E6
     border: solid 1px black
