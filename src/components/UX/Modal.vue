538dd309de912470be4d65acc265d3c7b56b8c66
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-07-09 16:46:30 +0200

    Components - UX - Creating Modal component
    Modals will be displayed whenever their associated button is clicked.
    Here we use it to display help about how to use the app.

---
+++ src/components/UX/Modal.vue
@@ -0,0 +1,90 @@
+<template>
+  <div
+    v-if="isVisible"
+    class="modal-container"
+  >
+    <div
+      :id="`modal-${modal.name}`"
+      class="modal"
+    >
+      <div class="modal-header">
+        <h3>{{ title }}</h3>
+        <div @click="close">
+          <i class="fas fa-times-circle"></i>
+        </div>
+      </div>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modal: { type: Object, default: () => {} }
+  },
+  computed: {
+    isVisible () { return this.modal.visible || false },
+    title () { return this.modal.content.title }
+  },
+  methods: {
+    close () {
+      this.modal.visible = false
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.modal-container
+  z-index: 1000
+  position: absolute
+  top: 0
+  left: 0
+  width: 100%
+  height: 100%
+  display: flex
+  background-color: #2A353BBB
+
+.modal
+  position: relative
+  align-self: center
+  margin: auto
+  height: 90%
+  width: 70%
+  border: solid 1px #EFEFEF
+  border-radius: 4px
+  color: #EFEFEF
+  background-color: #2A353B
+  box-shadow: 10px 5px 5px #2A353B
+
+.modal-header, .modal-body, .modal-footer
+  padding: 10px
+
+.modal-header, .modal-footer
+  background-color: #0BB4F5
+
+.modal-header
+  display: flex
+  justify-content: space-between
+  border-top-left-radius: 4px
+  border-top-right-radius: 4px
+  h3
+    margin: 0
+  div
+    height: fit-content
+    align-self: center
+    &:hover
+      cursor: pointer
+
+.modal-footer
+  position: absolute
+  bottom: 0
+  left: 0
+  right: 0
+  border-bottom-left-radius: 4px
+  border-bottom-right-radius: 4px
+</style>
