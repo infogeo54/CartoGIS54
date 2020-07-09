d85a2938f569be77ae5540b06d49b472fe4a64bb
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-07-09 11:12:30 +0200

    Header - Creating Button component
    This component will be used to create a header Button

---
+++ src/components/Header/Buttons/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <div @click="clicked">
+    <i :class="button.icon"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+      button: { type: Object, default: () => {} }
+  },
+  methods: {
+      clicked () { this.$emit('clicked', this.button) }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+svg
+  top: 50%
+  height: 32px
+  width: auto !important
+  margin: 15px
+  color: #2A353B
+  &:hover
+   cursor: pointer
+   color: #565656
+</style>
