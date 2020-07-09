e92b5962461d3c298d2697f624d82dac4903e335
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-07-09 11:20:28 +0200

    Layout - Including Header into the Default layout
    Header is the new name of the TopBar component

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,34 +1,34 @@
 <template>
   <div class="default-layout">
-    <TopBar />
+    <Header />
     <slot />
   </div>
 </template>
 
 <script>
-    import TopBar from '@/components/TopBar'
+import Header from '@/components/Header'
 
-    export default {
-        name: 'default-layout',
-        components: {
-            TopBar
-        }
+export default {
+    name: 'default-layout',
+    components: {
+        Header
     }
+}
 </script>
 
 <style lang="sass">
-  html
-    height: 100vh
-    body
-      margin: 0
-      height: 100%
-    .default-layout
-      height: 100%
-      display: grid
-      grid-template-rows: repeat(10, 10%)
-      font-family: Arial, 'sans-serif'
-    .top-bar
-      grid-row: 1 / 2
-    main
-      grid-row: 2 / 11
+html
+  height: 100vh
+  body
+    margin: 0
+    height: 100%
+  .default-layout
+    height: 100%
+    display: grid
+    grid-template-rows: repeat(10, 10%)
+    font-family: Arial, 'sans-serif'
+  .top-bar
+    grid-row: 1 / 2
+  main
+    grid-row: 2 / 11
 </style>
