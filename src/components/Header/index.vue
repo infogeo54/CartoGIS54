048647b4c673d2d728c009285b3eca52cd9bd982
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-07-09 11:19:46 +0200

    Header - Turning TopBar component into Header
    This is the component where header data and event listeners will be defined.
    It's now composed of a Brand logo on the left and a Buttons component on the right

---
+++ src/components/Header/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header">
+    <a href="http://meurthe-et-moselle.fr">
+      <img src="@/assets/design/meurthe-et-moselle_blue.png" alt="Meurthe et Moselle">
+    </a>
+    <Buttons :buttons="buttons" />
+  </div>
+</template>
+
+<script>
+import Buttons from './Buttons'
+
+export default {
+    name: 'Header',
+    components: { Buttons },
+    data () {
+        return {
+            buttons: [
+                {
+                    name: 'help',
+                    icon: 'fas fa-question-circle'
+                },
+                {
+                    name: 'user',
+                    icon: 'fas fa-user-circle'
+                }
+            ]
+        }
+    },
+}
+</script>
+
+<style lang="sass" scoped>
+.header
+  display: flex
+  justify-content: space-between
+  align-items: center
+  background-color: #0BB4F5
+  border-bottom: solid 1px #2A353B
+  a, img
+    height: 100%
+  a
+    max-width: 150px
+</style>
