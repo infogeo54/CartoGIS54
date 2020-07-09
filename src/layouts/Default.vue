2eef55566dc4b72723f2e990fe1e9d4f98403433
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-07-09 16:44:22 +0200

    Layout - Removing Header component & Removing grid display

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="default-layout">
-    <Header />
     <slot />
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header'
-
 export default {
     name: 'default-layout',
-    components: {
-        Header
-    }
 }
 </script>
 
@@ -24,11 +18,4 @@ html
     height: 100%
   .default-layout
     height: 100%
-    display: grid
-    grid-template-rows: repeat(10, 10%)
-    font-family: Arial, 'sans-serif'
-  .top-bar
-    grid-row: 1 / 2
-  main
-    grid-row: 2 / 11
 </style>
