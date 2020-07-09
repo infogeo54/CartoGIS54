43fbafa8f2717d83fb72f792d361182e31a436e8
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-07-09 09:48:00 +0200

    Form - Using store's cancel action & cleaning
    The Form component is now using store's action instead of making the treatment itself.
    Furthermore, we can delete useless getters

--- src/components/Form/index.vue
+++ src/components/Form/index.vue
@@ -16,84 +16,77 @@
 </template>
 
 <script>
-    import Group from './Group'
-    import {mapGetters, mapMutations, mapActions} from 'vuex'
+import Group from './Group'
+import {mapGetters, mapMutations, mapActions} from 'vuex'
 
-    export default {
-        name: "Form",
-        components: { Group },
-        computed: {
-            ...mapGetters({
-                map: 'map',
-                layer: 'layer/selected',
-                feature: 'feature/selected',
-                ogProperties: 'feature/ogProperties'
-            }),
-            properties: function () {
-                return Object.keys(this.feature.properties).sort((a, b) => a < b ? -1 : a > b ? 1 : 0)
-            }
-        },
-        methods: {
-            ...mapMutations('feature', ['updateAttribute']),
-            ...mapActions([
-                'reset',
-                'feature/save',
-                'feature/delete'
-            ]),
-            /**
-             * Saves changes in the Store
-             * @param attribute
-             */
-            onChange: function (attribute) {
-                this.updateAttribute(attribute)
-            },
-            /**
-             * Call store's save action
-             */
-            onSaveClick: async function () {
-                await this['feature/save']()
-            },
-            /**
-             * Call store's delete action
-             */
-            onDeleteClick: async function () {
-                await this['feature/delete']()
-            },
-            /**
-             * Cancel the current editing
-             * Reset an existing Feature to it's original state
-             */
-            onCancelClick: function () {
-                this.feature.deleteRepresentation()
-                if (this.ogProperties) {
-                    this.feature.properties = this.ogProperties
-                    this.feature.createRepresentation().addTo(this.map)
-                }
-                this.$destroy()
-            },
-            /**
-             * Call the appropriated action then destroy the component
-             * @param e : Event - An event created by clicking on a form's button
-             */
-            action: async function (e) {
-                switch (e.submitter.name) {
-                    case 'save':
-                        await this.onSaveClick()
-                        break
-                    case 'delete':
-                        if (confirm('Êtes-vous sûr.e de vouloir supprimer cette entité ?')) await this.onDeleteClick()
-                        break
-                }
-                this.$destroy()
-            }
+export default {
+    name: "Form",
+    components: { Group },
+    computed: {
+        ...mapGetters({
+            feature: 'feature/selected',
+        }),
+        properties: function () {
+            return Object.keys(this.feature.properties).sort((a, b) => a < b ? -1 : a > b ? 1 : 0)
+        }
+    },
+    /**
+     * Call store's reset action before destroying the component
+     */
+    beforeDestroy() {
+        this.reset()
+    },
+    methods: {
+        ...mapMutations('feature', ['updateAttribute']),
+        ...mapActions([
+            'reset',
+            'feature/save',
+            'feature/delete',
+            'feature/cancel'
+        ]),
+        /**
+         * Saves changes in the Store
+         * @param attribute
+         */
+        onChange: function (attribute) {
+            this.updateAttribute(attribute)
         },
         /**
-         * Call store's reset action before destroying the component
+         * Call store's save action
          */
-        beforeDestroy() {
-            this.reset()
+        onSaveClick: async function () {
+            await this['feature/save']()
+        },
+        /**
+         * Call store's delete action
+         */
+        onDeleteClick: async function () {
+            await this['feature/delete']()
+        },
+        /**
+         * Call store's cancel action
+         */
+        onCancelClick: function () {
+            this['feature/cancel']()
+            this.$destroy()
+        },
+        /**
+         * Call the appropriated action then destroy the component
+         * @param e : Event - An event created by clicking on a form's button
+         */
+        action: async function (e) {
+            switch (e.submitter.name) {
+                case 'save':
+                    await this.onSaveClick()
+                    break
+                case 'delete':
+                    if (confirm('Êtes-vous sûr.e de vouloir supprimer cette entité ?')) await this.onDeleteClick()
+                    break
+            }
+            this.$destroy()
         }
     }
+}
 </script>
 
 <style lang="sass" scoped>
