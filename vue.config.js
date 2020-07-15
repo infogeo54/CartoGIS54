6c212c12422219002522bfe98928ffcc38e3e694
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-07-15 09:30:02 +0200

    Configuring WebPack to load HTML files

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-    publicPath: '/'
+    publicPath: '/',
+    chainWebpack: config => {
+      config.module
+        .rule('html')
+        .test(/\.html$/)
+        .use('html-loader')
+          .loader('html-loader')
+          .end()
+        .end()
+
+    }
 }
